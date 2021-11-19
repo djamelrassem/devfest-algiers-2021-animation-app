d56e1169042ba7da969de62c8f2224608f09b35a
Lamouri Djamel Rassem <lamouridjamelrassem@gmail.com>
2021-11-19 23:09:24 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  return runApp(DevFestApp());
+}
+
+class DevFestApp extends StatelessWidget {
+  const DevFestApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
